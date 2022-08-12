9cd47169dfb7bf4b79a8b98a2ec0ac9b8eafdae0
Garrett Hays <GarrettlHays@gmail.com>
2022-08-12 11:24:19 -0700

    adds home controller file to project

---
+++ SweetsTreats/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SweetsTreats.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SweetsTreats.Controllers
+{
+    public class HomeController : Controller
+    {
+      private readonly SweetsTreatsContext _db;
+
+      public HomeController(SweetsTreatsContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        ViewBag.Treats = _db.Treats.ToList();
+        ViewBag.Flavors = _db.Flavors.ToList();
+        return View();
+      }
+
+    }
+}
